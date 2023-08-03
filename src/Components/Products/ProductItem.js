48121f78424cd84bc94e3a1f256310edf028bbbf
aisankheiri <aisankheiri@marun.edu.tr>
2023-08-03 19:02:23 +0300

    add button on image

--- src/Components/Products/ProductItem.js
+++ src/Components/Products/ProductItem.js
@@ -6,6 +6,7 @@ const ProductItem = ({productItems}) => {
     <div className="product-category" >
     <div className="product-boxes-img-container">
       <img className="product-image" src={productItems.image} alt={productItems.name} />
+      <button className='add-to-cart-btn'>+</button>
     </div>
     <h2 className='product-price'>₺ {productItems.price}</h2>
     <h3 className='product-name'>{productItems.name}</h3>
