5c02dc722f96a9e406026c1bc12286292ae44bc9
aisankheiri <aisankheiri@marun.edu.tr>
2023-08-14 18:28:42 +0300

    add down icon

--- src/Routes/Getir/Getir.js
+++ src/Routes/Getir/Getir.js
@@ -3,13 +3,16 @@ import Navbar from "../../Components/Navbar/Navbar";
 import Slider from "../../Components/Slider/Slider";
 import Kategoriler from "../../Components/Kategoriler/Kategoriler";
 import NavbarBelow from "../../Components/NavbarBelow/NavbarBelow";
+import "./Getir.css"
 const Getir = ({cartItems, setCartItems}) => {
   return (
     <div>
       <Navbar />
+      <div className="for-back-color">
       <Slider />
       <Kategoriler cartItems={cartItems} setCartItems={setCartItems}/>
       <NavbarBelow/>
+      </div>
  
       
     </div>
